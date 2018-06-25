1144dce4102dfe462deac174603d53a7d5861c16
minh15662 <minh.milano1511@gmail.con>
2018-06-25 14:08:16 +0700

    Exersice 1-10

--- ThanhMinh/Bai10.js
+++ ThanhMinh/Bai10.js
@@ -1,13 +1,11 @@
 /*Write a JavaScript program to calculate multiplication and division of two numbers (input from user).*/
 	function mutiplyBy(a,b){
-		var num1=a;
-		var num2=b;
-		 return num1*num2;
+		var num=a*b;
+		 return num;
 	
 	}
 	function divideBy(a,b){
-		var num1=a;
-	`	var num2=b;
-		return num1/num2;
+		var num=a/b;
+		 return num;
 	}
 
