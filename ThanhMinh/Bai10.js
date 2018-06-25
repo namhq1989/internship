04ae4eb9e00b149af0a2b8647655c424cdeddb33
minh15662 <minh.milano1511@gmail.con>
2018-06-25 09:01:14 +0700

    Execise 1-10

--- ThanhMinh/Bai10.js
+++ ThanhMinh/Bai10.js
@@ -1,11 +1,13 @@
 /*Write a JavaScript program to calculate multiplication and division of two numbers (input from user).*/
 	function mutiplyBy(a,b){
-		var num=a*b;
-		 return num;
+		var num1=a;
+		var num2=b;
+		 return num1*num2;
 	
 	}
 	function divideBy(a,b){
-		var num=a/b;
-		 return num;
+		 var num1=a;
+	`		var num2=b;
+		 return num1/num2;
 	}
 
--- ThanhMinh/bai5.js
+++ ThanhMinh/bai5.js
@@ -1,5 +1,6 @@
 /*. Write a JavaScript program to rotate the string 'w3resource' in right direction by periodically removing one letter from the end of the string and attaching it to the front. */
-				function  animate_string(element){
+				function  animate_string(id){
+					var element=document.getElementById(id);
 					var textNode=element.childNodes[0];
 					var text= textNode.data;
 				setInterval(function(){
