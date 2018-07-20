c985aee63173fded89a2e44fb5127d0481b3b9a4
luuvansinh <luuvansinh555@gmail.com>
2018-07-20 15:14:22 +0700

    fix layout responsive

--- zody-merchant/src/screens/statistic/ statistical-card.js
+++ zody-merchant/src/screens/statistic/ statistical-card.js
@@ -17,7 +17,7 @@ class StatisticalCard extends React.Component {
       </div>
     )
     return (
-      <Col xs={24} sm={12} md={8} lg={8} xl={8} className={style.colCard}>
+      <Col xs={24} sm={24} md={12} lg={8} xl={8} className={style.colCard}>
         <Card>
           {cardTitle}
           <img src={icon} alt="" className={style.cardIcon} />
